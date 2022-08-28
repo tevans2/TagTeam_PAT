aedfc7ab87a258e849594b0de3cc7ca11f03b2e9
tevans@westerford.co.za <tevans2>
2022-08-28 15:05:18 +0200

    test comment

--- SQL_CONNECTION_u.pas
+++ SQL_CONNECTION_u.pas
@@ -38,7 +38,7 @@ procedure TDemonstration.connectButtonClick(Sender: TObject);
 begin
   SQLConnection1.Params.Add('Database=tagteam.sqlite3');
   try
-    SQLConnection1.Connected := true;            //test
+    SQLConnection1.Connected := true;            //test test
     executeButton.Enabled := true;
     outputMemo.Text := 'Connection Established!';
   except
